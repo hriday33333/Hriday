c52f877bfc657d9f9c622e57c26568b1be4b7027
Hriday <topuk864@gmail.com>
2026-08-09 12:53:55 +0600

    name

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -171,7 +171,7 @@ export const Footer = () => {
             viewport={{ once: true }}
           >
             <div>
-              <p>© {currentYear} Sahil. All rights reserved.</p>
+              <p>© {currentYear} Hriday. All rights reserved.</p>
             </div>
 
             <div className="flex items-center space-x-6">
